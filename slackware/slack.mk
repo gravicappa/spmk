67a55ec9939ce86b146c89c1839fa854f0b24971
Ramil Farkhshatov <ramil@gmx.co.uk>
2013-07-16 19:45:16 +0400

    Simplified falling back to slackware own packages in slackware backend.

---
+++ slackware/slack.mk
@@ -0,0 +1,8 @@
+%:Q:
+  _found=0
+  for (_x in /var/log/packages/$target-[0-9]*)
+    if (test -e $_x)
+      _found=1
+  if (~ $_found 0)
+    $sudo slapt-get -i $target || $sudo slackpkg install $target
+  if not true
